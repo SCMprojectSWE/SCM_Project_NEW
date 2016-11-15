00e09871c67fe6bdae3d1e09fec158bd514b0a29
BAIDU\Wich <wichersleyer@gmail.com>
2016-11-15 18:29:21 +0700

    โปรแกรมคำนวณค่าตอบแทนของพนักงาน ซึ่งโปรแกรมได้มีการแก้ไข ส่วนของ ชื่อ class และ ชื่อ constucter และ ชื่อ Method เพื่อที่จะให้สื่อความหมายมากกว่า และได้มีการเปลี่ยนแปลง ในส่วนของไฟล์ Calculat_compensation ได้มีการแก้ไขในส่วนของ New option ให้มีชื่อเหมือนในส่วนของไฟล์ Calculat ที่ได้ทำแก้ไขไปข้างต้น. อธิบายโดย นายสิรวิชญ์  วาสนาทิพย์

--- Calculate_Compensation/Calculat_compensation.java
+++ Calculate_Compensation/Calculat_compensation.java
@@ -5,26 +5,37 @@ import java.util.Scanner;
 public class Calculat_compensation {
 
 	public static void main(String[] args) {
-		 Scanner input = new Scanner(System.in);
-		 System.out.print("Input name: ");
-	     String name = input.nextLine();
-	      System.out.print("Input Salary: ");
-	     double salary = input.nextDouble();
-	      System.out.print("Input Monthly sales: ");
-	      double sales = input.nextDouble();
-	      Calculat cal = new Calculat(name,salary,sales);
-	      if(cal.getsales()>=100001){
-	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com1()); 
-	      }
-	      else if (cal.getsales()>=50001){
-	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com2()); 
-	      }
-	      else{
-	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com3()); 
-	      }
-	}
+	Scanner input = new Scanner(System.in);
+	 System.out.print("Input name: ");
+     String name = input.nextLine();
+     System.out.print("Input Salary: ");
+    double salary = input.nextDouble();
+     System.out.print("Input Monthly sales: ");
+     double sales = input.nextDouble();
+     Calculate cal = new Calculate(name,salary,sales);
+     if(cal.getsalaty()>=100001){
+    	 if(cal.Calculat_compensation1()<18000){
+    		 System.out.println("Name Employee is "+cal.getname()+"   compensation is : 18000 " ); 
+         }else{
+   	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_compensation1()); 
+         }
+     } else if (cal.getsales()>=50001){
+    	 if(cal.Calculat_compensation2()<18000){
+    		 System.out.println("Name Employee is "+cal.getname()+"   compensation is : 18000 " ); 
+         }else{
+   	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_compensation2()); 
+         }
+     
+     }else{
+    	 if(cal.Calculat_compensation3()<18000){
+    		 System.out.println("Name Employee is "+cal.getname()+"   compensation is : 18000 " ); 
+         }else{
+   	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_compensation3()); 
+         }
+     }
+     
+    
+
 }
-
-
-
+}
 
--- Calculate_Compensation/Calculate.java
+++ Calculate_Compensation/Calculate.java
@@ -1,13 +1,13 @@
-package Project;
+ppackage Project;
 
 import javax.swing.Spring;
 
-public class Calculat {
+public class Calculate {
 
 	private String name;
 	private double salary;
 	private double sales;
-	public Calculat(String name,double salary,double sales){
+	public Calculate(String name,double salary,double sales){
 		this.name=name;
 		this.salary=salary;
 		this.sales=sales;
@@ -23,18 +23,18 @@ public class Calculat {
 	public double getsales(){
 		return sales;	
 	}
-	public double Calculat_com1(){
+	public double Calculat_compensation1(){
 		double com;
 		 com =(salary+(sales *0.03));
 		 return com;
 	}
-	public double Calculat_com2(){
+	public double Calculat_compensation2(){
 		double com;
 		 com =(salary+(sales *0.02));
 		 return com;
 	}
 	
-	public double Calculat_com3(){
+	public double Calculat_compensation3(){
 		double com;
 		 com =(salary+(sales *0.01));
 		 return com;
