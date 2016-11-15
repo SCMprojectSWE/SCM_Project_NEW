67cf4f08cb8b98440e74b22bb3b7a565c406e406
Poottida <Poottida57143521@gmail.com>
2016-11-15 18:16:13 +0700

    ทำการแก้ไขโปรแกรมคำนวนค่าตอบแทนของพนักงานในส่วนของการเรียก new option การเรียก method จากคลาส Culculat.java และ condition ได้มีการใส่เครื่องหมายที่ไม่ถูกต้องจึงทำให้โปรแกรมทำงานผิดพลาด อธิบายโดย นางาวพุธธิดา จิตรธรรม

--- Calculate_Compensation/Calculat_compensation.java
+++ Calculate_Compensation/Calculat_compensation.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class Calculat_compensation {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		 Scanner input = new Scanner(System.in);
 		 System.out.print("Input name: ");
-	      String name = input.nextLine();
+	     String name = input.nextLine();
 	      System.out.print("Input Salary: ");
 	     double salary = input.nextDouble();
 	      System.out.print("Input Monthly sales: ");
 	      double sales = input.nextDouble();
-	      if(sales>100001){
-	    	  double com =(salary+(sales *0.03));
-	    	  System.out.println("Name Employee is "+name+"   compensation is : " + com); 
+	      Calculat cal = new Calculat(name,salary,sales);
+	      if(cal.getsales()>=100001){
+	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com1()); 
 	      }
-	      else if (sales>50001){
-	    	  double com =(salary+(sales *0.02));
-	    	  System.out.println("Name Employee is "+name+"   compensation is : " + com); 
+	      else if (cal.getsales()>=50001){
+	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com2()); 
 	      }
 	      else{
-	    	  double com =(salary+(sales *0.01));
-	    	  System.out.println("Name Employee is "+name+"   compensation is : " + com); 
+	    	  System.out.println("Name Employee is "+cal.getname()+"   compensation is : " + cal.Calculat_com3()); 
 	      }
 	}
 }
